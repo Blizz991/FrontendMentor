5e3c40447725f734e15c429772f72d0282ce16a1
Blizz991 <martinhblizz@gmail.com>
2023-05-12 10:08:43 +0200

    Initial solution setup

---
+++ interactive-comments-section-main/solution/nuxt.config.ts
@@ -0,0 +1,11 @@
+import { defineNuxtConfig } from 'nuxt/config'
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
