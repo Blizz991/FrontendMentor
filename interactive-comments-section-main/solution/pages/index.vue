c2c754619e4381a564008d79936aec2cacd34b90
Blizz991 <martinhblizz@gmail.com>
2023-05-18 09:26:17 +0200

    Initial setup to get content on a separate page

--- interactive-comments-section-main/solution/pages/index.vue
+++ interactive-comments-section-main/solution/pages/index.vue
@@ -1,7 +1,25 @@
+<script setup>
+//The following code returns the data correct, but also wraps it in an array
+//Which leads to having to specify data[0] to get the single object, which seems silly.
+// const { data } = await useAsyncData('home', () =>
+//   queryContent('/').find()
+// );
+
+//As such I went with this, but I would assume there's a better way to get the content
+//How ever, this gets us the full json object directly.
+const { data } = await useAsyncData('content-/data', () =>
+  queryContent().where({ _path: '/data' }).findOne()
+);
+
+</script>
 <template>
   <section>
     <h2>This is the default page content</h2>
+    <h3>Hello test {{ data.currentUser.username }}</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore non dolorem officiis debitis, nihil optio inventore
       expedita ullam nemo cumque temporibus quidem ut voluptates nam! Totam tenetur fugiat reprehenderit nostrum!</p>
+      <pre>
+        {{ data }}
+      </pre>
   </section>
 </template>
