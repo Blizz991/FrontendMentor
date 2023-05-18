9cb3f4f4b8515fb63460102f2fb712b51eab9dd1
Blizz991 <martinhblizz@gmail.com>
2023-05-18 08:40:04 +0200

    Setup simple page / layout

---
+++ interactive-comments-section-main/solution/layouts/default.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>This is a default layout shared across all pages</h1>
+  <slot></slot>
+</template>
---
+++ interactive-comments-section-main/solution/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <section>
+    <h2>This is the default page content</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore non dolorem officiis debitis, nihil optio inventore
+      expedita ullam nemo cumque temporibus quidem ut voluptates nam! Totam tenetur fugiat reprehenderit nostrum!</p>
+  </section>
+</template>
