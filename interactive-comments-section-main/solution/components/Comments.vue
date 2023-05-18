32caf1bb17b610077abf2503debf763c2fcbf9e5
Blizz991 <martinhblizz@gmail.com>
2023-05-18 17:11:55 +0200

    Fix hole in reply border

--- interactive-comments-section-main/solution/components/Comments.vue
+++ interactive-comments-section-main/solution/components/Comments.vue
@@ -2,7 +2,7 @@
   <ul class="max-w-[720px]">
     <li v-for="comment in data.comments" class="mb-5 last-of-type:mb-0">
       <Comment :comment="comment" />
-      <ul v-for="reply in comment.replies" class="mt-5 ml-10 pl-10 border-l-2">
+      <ul v-for="reply in comment.replies" class="ml-10 pt-5 pl-10 border-l-2 first-of-type:pt-0 first-of-type:mt-5">
         <li class="mb-5 last-of-type:mb-0">
           <Comment :comment="reply" :currentUsername="data.currentUser.username" />
         </li>
