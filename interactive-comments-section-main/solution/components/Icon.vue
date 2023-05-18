1535056dc5f0bb77f95a967abec60211cf201b98
Blizz991 <martinhblizz@gmail.com>
2023-05-18 12:39:29 +0200

    Removed icons from static as I'll use inline

--- interactive-comments-section-main/solution/components/Icon.vue
+++ interactive-comments-section-main/solution/components/Icon.vue
@@ -1,4 +1,5 @@
 <template>
   <!-- I've decided not to go this route, but leaving this here in case I change my mind -->
   <!-- Inspiration if I want to move away from inline SVG: https://jessarcher.com/articles/creating-an-icon-component-in-vue/ -->
+  <!-- Performance comparissons https://cloudfour.com/thinks/svg-icon-stress-test/ -->
 </template>
