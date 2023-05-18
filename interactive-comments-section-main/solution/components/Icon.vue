d995f5397fb3f3cdf6c694771eed0efbc1fef30a
Blizz991 <martinhblizz@gmail.com>
2023-05-18 11:41:38 +0200

    Some notes for icon component (not used)

---
+++ interactive-comments-section-main/solution/components/Icon.vue
@@ -0,0 +1,4 @@
+<template>
+  <!-- I've decided not to go this route, but leaving this here in case I change my mind -->
+  <!-- Inspiration if I want to move away from inline SVG: https://jessarcher.com/articles/creating-an-icon-component-in-vue/ -->
+</template>
